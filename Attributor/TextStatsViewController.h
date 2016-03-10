25c463f270a4d5dcf91b3c568c8d3c4491af6d6c
[David Anglin] <[DAAnglin6389@gmail.com]>
2016-03-10 15:21:52 -0800

    added segue with stats

---
+++ Attributor/TextStatsViewController.h
@@ -0,0 +1,15 @@
+//
+//  TextStatsViewController.h
+//  Attributor
+//
+//  Created by David Anglin on 3/10/16.
+//  Copyright © 2016 David Anglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextStatsViewController : UIViewController
+
+@property (nonatomic, strong) NSAttributedString *textToAnalyze;
+
+@end
